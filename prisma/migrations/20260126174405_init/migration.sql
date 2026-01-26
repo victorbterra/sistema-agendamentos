d396b1fe0e9518c47fd15340ee6faa0c373fa393
Victor Terra <victorterradev@gmail.com>
2026-01-26 15:28:22 -0300

    feat: integração com Prisma e SQLite para persistência de agendamentos

---
+++ prisma/migrations/20260126174405_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Agendamento" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cliente" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "hora" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
--- server.js
+++ server.js
@@ -1,26 +1,47 @@
 import express from "express";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
 const app = express(); // cria uma instância express
 const PORT = process.env.PORT || 3336
 
-const agendamentos = [];
+const prisma = new PrismaClient(); // cria uma instância do Prisma Client   
+
+
 
 app.use(express.json()) // Diz ao servidor para entender mensagens em json
 
 
-app.post('/agendar', (req,res)=>{
+app.post('/agendar',async (req,res)=>{
     const {cliente, data, hora} = req.body;
 
-    console.log("Recebido", {cliente, data, hora});
-
-    const conflito = agendamentos.find(agendamento => agendamento.data === data && agendamento.hora === hora);
-
-    if(conflito){
-        return  res.status(400).json({message: "Conflito: Horário já agendado"});
+    try{
+        // Verificação de conflito de agendamento
+        const conflito = await prisma.Agendamento.findFirst({
+            where:{
+                data,
+                hora
+            }
+        });
+        if(conflito){
+            return  res.status(400).json({message: "Conflito: Horário já agendado"});
+        }
+        // Criação do agendamento
+        const novoAgendamento = await prisma.Agendamento.create({
+            data:{
+                cliente,
+                data,
+                hora
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message: "Erro interno no servidor"});
     }
+
+
 
     agendamentos.push({cliente, data, hora});
     res.status(201).json({message: "Agendamento realizado com sucesso"});
